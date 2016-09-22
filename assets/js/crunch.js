73dd62dc64d9ed47b7e5c1046910c7a57447c6ff
Stella Wu <stellawu@utexas.edu>
2016-09-21 19:17:21 -0500

    able to pin the start point and search nearby restaurants with keyterms within 500 meters and return markeres with info windows of restaurant names

--- assets/js/crunch.js
+++ assets/js/crunch.js
@@ -1,56 +1,115 @@
-var yelpResults;
-
 $(document).ready(function() {
     $('select').material_select();
 
+	//initial map
+	var map;
+	var geocoder;
+	var infowindow;
+	function myMap() {
+	  var mapCanvas = document.getElementById("map");
+	  var mapOptions = {
+	    center: new google.maps.LatLng(30.2669444,-97.7427778),
+	    zoom: 12
+	  };
+	 map = new google.maps.Map(mapCanvas, mapOptions);
+	}
+	myMap();
 
-  // MAP
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(30.2669444,-97.7427778),
-    zoom: 12
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+	//autocomplete for currentLoc
+	var defaultBounds = new google.maps.LatLngBounds(
+	  new google.maps.LatLng(30.136290,-97.945014),
+	  new google.maps.LatLng(30.536753, -97.559441));
+	var input = document.getElementById('currentLoc');
+	var options = {
+	  bounds: defaultBounds,
+	  types: ['address']
+	};
+	var autocomplete = new google.maps.places.Autocomplete(input, options);
+	autocomplete.addListener('place_changed', fillInAddress);
 
-  // AUTO-COMPLETE
-  var defaultBounds = new google.maps.LatLngBounds(
-    new google.maps.LatLng(-124.626080, 18.005611),
-    new google.maps.LatLng(-62.361014, 48.987386));
-  var placeInput = document.getElementById("currentLoc");
-  var options = {
-    bounds: defaultBounds,
-    types: ['address']
-  };
-  var autocomplete = new google.maps.places.Autocomplete(placeInput, options);
-  autocomplete.addListener('place_changed', fillInAddress);
+	function fillInAddress() {
+	  // Get the place details from the autocomplete object.
+	  var place = autocomplete.getPlace();
+	  console.log(place);
+	}
+	// on click, mark startpoint with a marker and recenter to currentLoc
 
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
-    console.log(place);
-  }
+	function codeAddress(){
+	  geocoder = new google.maps.Geocoder();
+	  var address = $('#currentLoc').val().trim();
+	  geocoder.geocode({'address':address},function(results, status){
+	    if (status =='OK') {  
+	      var marker = new google.maps.Marker({position:results[0].geometry.location,
+	       animation:google.maps.Animation.DROP,
+	    	icon:'http://icons.iconarchive.com/icons/david-renelt/little-icon-people/32/Women-icon.png'});
+	      marker.setMap(map);
+	      map.setCenter(results[0].geometry.location);  
+	      map.setZoom(16);
+	      map.center = results[0].geometry.location;
+	      //search nearby restaurants by key terms and set markers and infowindows
+	        var keyTerm = $('#foodCategory').val().trim();
+	        infowindow = new google.maps.InfoWindow();
+		  	var service = new google.maps.places.PlacesService(map);
+		      service.nearbySearch({
+		          location: map.center,
+		          radius: 500,
+		          type: ['restaurant'],
+		          name: keyTerm
+		        }, callback);
+		      function callback(results, status) {
+		        if (status === google.maps.places.PlacesServiceStatus.OK) {
+		          for (var i = 0; i < results.length; i++) {
+		            createMarker(results[i]);
+		          }
+		        }
+		      }
+		      function createMarker(place) {
+		        var placeLoc = place.geometry.location;
+		        var marker = new google.maps.Marker({
+		          map: map,
+		          position: place.geometry.location
+		        });
 
-  $('#submitButton').on('click', function() {
-    // We use a promise because getYelp is asynchronous 
-    var promise = getYelp($('#foodCategory').val().trim(), $('#currentLoc').val().trim());
+		        google.maps.event.addListener(marker, 'click', function() {
+		          infowindow.setContent(place.name);
+		          infowindow.open(map, this);
+		        });
+		      }
+	    }
+	  })
+	};
 
-    promise.success(function (data) {
 
-      yelpResults = data.businesses;
-      
-      var latitude = yelpResults[0].location.coordinate.latitude;
-      var longitude = yelpResults[0].location.coordinate.longitude;
-      var image = yelpResults[0].image_url;
-      var name = yelpResults[0].name;
-      var url = yelpResults[0].url;
-      var stars = yelpResults[0].rating;
 
-      // yelpResults.splice(0,1);
+	//marker
+	$('#search').on('click',function(){
+	  codeAddress();
 
-    });
+	  //distance matrix
+		var startPoint = $('#currentLoc').val().trim();
+		var origins = [startPoint];
+		//get restaurant coordinates from yelp callback 'endpoint'
+		
+		var endPoint;
+		var destinations;
+		var travel = $('#travel').val().trim().toUpperCase();
+	
 
-  })
+		var distanceMatrix  = new google.maps.DistanceMatrixService();
+		var distanceRequest = { origins: origins, destinations: destinations, travelMode: google.maps.TravelMode[travel], unitSystem: google.maps.UnitSystem.IMPERIAL, avoidHighways: false, avoidTolls: false };
 
+		distanceMatrix.getDistanceMatrix(distanceRequest, function(response, status) {
+		    if (status != google.maps.DistanceMatrixStatus.OK) {
+		        alert('Error was: ' + status);
+		    }
+		    else {
+		        var origins      = response.originAddresses;
+		        var destinations = response.destinationAddresses;
+		        console.log(response);
+		        console.log('Distance: '+ response.rows[0].elements[0].distance.text);
+		        console.log('Duration: '+response.rows[0].elements[0].duration.text);
+		    }
+  		});
+
+	});
 });
-
-
