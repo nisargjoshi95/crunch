04ee5d56d53fa9bf295a89690e9a5fa22be88c63
Mitchel Wodka <mrw.simple@gmail.com>
2016-09-26 21:32:21 -0500

    added geolocation

--- assets/js/crunch.js
+++ assets/js/crunch.js
@@ -5,6 +5,32 @@ $(document).ready(function() {
 	$('select').material_select();
 
 	//initial map
+	var infoWindow = new google.maps.InfoWindow({map: map});
+         if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    }
+    
 	var mapCanvas = document.getElementById("map");
 	var mapOptions = {
 		center: new google.maps.LatLng(30.2669444,-97.7427778),
